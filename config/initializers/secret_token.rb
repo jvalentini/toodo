85d45b1910cb9821f401c0ea58a3d5d5f6ab9bd9
Justin Valentini <justin.valentini@gmail.com>
2013-04-14 20:28:12 -0400

    Creating rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toodo::Application.config.secret_token = 'a32884337b0182303459a02e75fdfea204836720a730efbbe7e81aa813b49aaabc087f9bfbd69fc1601c87d5ddfc490fd08222ce392412c27e15722cbbf24dc6'
